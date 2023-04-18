67c1e457cb3a396dcb18b6f622c3b992e3bb970a
AndreaSimmons <andrealcariola@gmail.com>
2023-04-18 16:20:50 -0700

    Add files via upload

---
+++ orderEntryPhase1/src/orderEntryPhase1/OrderEntryPhase1.java
@@ -0,0 +1,40 @@
+// Andrea Simmons, CIS406, Order Entry Phase 1 Lab
+
+package orderEntryPhase1;
+
+public class OrderEntryPhase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Enter item number (alphanumeric).
+		
+		
+		// Enter item description (alphanumeric).
+		
+		
+		// Enter the item price (decimal).
+		
+		
+		// Enter quantity ordered (integer).
+		
+		
+		// Enter the total tax for the item (decimal).
+		
+		
+		// Enter the total discount for the item (decimal).
+		
+		
+		// Enter the net amount for the item (decimal).
+		
+		
+		// Display the item number, item description, item price, 
+		// quantity ordered, tax amount, discount amount, and net amount.
+		// Labels should be on one line and data on the second line.
+		// Use tabs to line up the columns.
+		
+
+	}
+
+}
